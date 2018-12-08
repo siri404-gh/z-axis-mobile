7e286c8bba4c984a2bbb46705446169d69452b6e
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-12-08 14:56:30 +0000

    domain = londonz.app

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <WebView
-        source={{ uri: 'https://www.sreeram.app' }}
+        source={{ uri: 'https://www.londonz.app' }}
         style={{ marginTop: 20 }} />
     );
   }
