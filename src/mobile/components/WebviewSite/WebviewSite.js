0816548b0ea6b83c2ec8fb66d6b8e783248be2ca
Sreeram Padmanabhan <sreeram.com@gmail.com>
2019-02-21 19:34:27 +0530

    feat: webview component refactor

--- src/mobile/components/WebviewSite/WebviewSite.js
+++ src/mobile/components/WebviewSite/WebviewSite.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-import { View, WebView } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  WebView,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
 import { registerForPushNotificationsAsync } from './push';
 
-class WebviewSite extends Component {
+const url = 'https://test.londonz.app'
+
+class Londonz extends Component {
   state = {
     token: '',
   }
@@ -20,21 +30,59 @@ class WebviewSite extends Component {
     return null;
   }
   render() {
-    return <WebView
-      ref={n => this.WebViewRef = n}
-      userAgent={"londonz"}
-      javaScriptEnabled={true}
-      domStorageEnabled={true}
-      injectedJavaScript={`window.token='${this.state.token}'`}
-      startInLoadingState={true}
-      renderLoading={this.renderLoading}
-      scrollEnabled={true}
-      bounce={false}
-      useWebKit={true}
-      source={{ uri: 'https://test.londonz.app' }}
-      // source={{ uri: 'https://londonz-stage.firebaseapp.com' }}
-      style={{ height: '100%', marginBottom: this.props.marginBottom }} />;
+    return (
+      <WebView
+        ref={n => this.WebViewRef = n}
+        userAgent={"Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        injectedJavaScript={`window.token='${this.state.token}'`}
+        startInLoadingState={true}
+        renderLoading={this.renderLoading}
+        scrollEnabled={true}
+        bounce={false}
+        useWebKit={true}
+        source={{ uri: url }}
+        style={{ height: '100%', marginBottom: this.props.marginBottom }} />
+    );
   }
 }
 
-export default WebviewSite;
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoaded: false,
+    }
+  }
+
+  render() {
+    if (this.state.isLoaded) {
+      return <Londonz />
+    }
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <View style={{ height: 0, width: 0, padding: 0 }}>
+          <WebView
+            source={{ uri: url }}
+            onLoad={() => this.setState({ isLoaded: true })} />
+        </View>
+          <ActivityIndicator size="large" color="#00ff00" hidesWhenStopped={false} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    background: '#000000',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    background: '#000000',
+  }
+})
