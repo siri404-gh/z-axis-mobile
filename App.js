4bbb576e291358a0a41d69513391bcc7710c9c92
Sreeram Padmanabhan <sreeram.com@gmail.com>
2019-02-24 17:51:22 +0530

    feat: refactor

--- App.js
+++ App.js
@@ -14,38 +14,11 @@ const BannerAd = ({ }) => showBanner ? <AdMobBanner
   onDidFailToReceiveAdWithError={() => console.log('error from ad')} /> : null;
 
 export default class App extends Component {
-  state = {
-    refreshing: false,
-  }
-
-  async showAds() {
-    if (!showInterstitial) return;
-    AdMobInterstitial.setAdUnitID(interstitial);
-    AdMobInterstitial.setTestDeviceID("EMULATOR");
-    await AdMobInterstitial.requestAdAsync();
-    await AdMobInterstitial.showAdAsync();
-  }
-
-  _onRefresh = () => {
-    // setTimeout(this.showAds, 0);
-    this.setState({ refreshing: true });
-    this.setState({ refreshing: false });
-  }
-
-  getRefreshControl() {
-    return null;
-    return <RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh} title={'Loading'} />;
-  }
-
   render() {
-    return <ScrollView
-      contentContainerStyle={{ flex: 1, paddingTop: 30, backgroundColor: '#000000' }}
-      scrollEnabled={true}
-      bounces={false}
-      refreshControl={this.getRefreshControl()}>
+    return <View
+      style={{ flex: 1, backgroundColor: '#000000' }}>
       <StatusBar barStyle="light-content" />
-      <WebviewSite refresh={this.state.refreshing} marginBottom={showBanner ? 44 : 0} />
-      <BannerAd />
-    </ScrollView>;
+      <WebviewSite />
+    </View>;
   }
 }
--- src/mobile/components/WebviewSite/WebviewSite.js
+++ src/mobile/components/WebviewSite/WebviewSite.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  WebView,
-  Alert,
-  ActivityIndicator,
-} from 'react-native';
+import { WebView, ActivityIndicator } from 'react-native';
 import { registerForPushNotificationsAsync } from './push';
 
 const url = 'https://test.londonz.app'
 
-class Londonz extends Component {
+export default class App extends Component {
   state = {
     token: '',
   }
+
   componentDidMount() {
     registerForPushNotificationsAsync()
       .then(d => {
@@ -23,66 +16,26 @@ class Londonz extends Component {
       })
       .catch(e => console.log('error', e));
   }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps && nextProps.refresh === false) this.WebViewRef.reload();
-  }
+
   renderLoading() {
-    return null;
+    return <ActivityIndicator style={{ height: '100%', backgroundColor: '#000000' }} size="large" color="#0000ff" hidesWhenStopped={false} />
   }
+
   render() {
     return (
-      <WebView
+        <WebView
         ref={n => this.WebViewRef = n}
         userAgent={"Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"}
         javaScriptEnabled={true}
         domStorageEnabled={true}
         injectedJavaScript={`window.token='${this.state.token}'`}
         startInLoadingState={true}
-        renderLoading={this.renderLoading}
         scrollEnabled={true}
+        renderLoading={this.renderLoading}
         bounce={false}
         useWebKit={true}
         source={{ uri: url }}
-        style={{ height: '100%', marginBottom: this.props.marginBottom }} />
+        style={{ height: '100%', background: '#000000' }} />
     );
   }
 }
-
-export default class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isLoaded: false,
-    }
-  }
-
-  render() {
-    if (this.state.isLoaded) {
-      return <Londonz />
-    }
-    return (
-      <View style={[styles.container, styles.horizontal]}>
-        <View style={{ height: 0, width: 0, padding: 0 }}>
-          <WebView
-            source={{ uri: url }}
-            onLoad={() => this.setState({ isLoaded: true })} />
-        </View>
-          <ActivityIndicator size="large" color="#00ff00" hidesWhenStopped={false} />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    background: '#000000',
-  },
-  horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    background: '#000000',
-  }
-})
